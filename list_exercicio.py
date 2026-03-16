054ea9d6f11f8b934b3b9950e3ad9ac4dd935cd8
Nicolle Silva <nicolle62387396@alunos.sc.senac.br>
2026-03-16 20:19:10 -0300

    Listas

---
+++ list_exercicio.py
@@ -0,0 +1,10 @@
+lista_compras = []
+print(lista_compras)
+
+lista_compras.append("laranja")
+lista_compras.append("uva")
+lista_compras.append("maçã")
+print(lista_compras)
+
+lista_compras.remove("uva")
+print(lista_compras)
--- listas.py
+++ listas.py
@@ -14,3 +14,20 @@ print("Após adicionar:", frutas)
 frutas.remove("uva")
 ultima = frutas.pop()
 print("Após remover 'uva' e pop():", frutas, "Última removida:", ultima)
+
+print("Listas original 'numeros':", numeros)
+print("Somas dos números:", sum(numeros))")
+print("Maior número:", max(numeros))
+print("Menor número:", min(numeros))
+numeros.reverse()
+print("Reversa:", numeros)
+numeros.sort()
+print("Ordenada crescente:", numeros)
+numeros.sort(reverse=True)
+print("Ordenada decrescente:", numeros)
+
+for fruta in frutas:
+    print("Fruta:", fruta)
+
+quadrados = [n * n for n in [1, 2, 3, 4, 5]]
+print("Quadrados", quadrados)
