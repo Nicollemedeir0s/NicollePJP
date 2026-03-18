7fc8243077466327297c3c2d4d13393ad004893d
Nicollemedeir0s <medeirosnicolle03@gmail.com>
2026-03-18 19:17:22 -0300

    Commit
    Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NicollePJP
+Repositório para as aulas da turma 36.10 no programa Jovem Programador - SENAC 2026
---
+++ listas.py
@@ -0,0 +1,16 @@
+frutas = ["maçã", "banana", "uva"]
+numeros = [1, 2, 3, 4]
+
+print("Primeira fruta:", frutas [0])
+print("Última fruta:", frutas [-1])
+
+frutas[1] = "banana-nanica"
+print("Após alterar:", frutas)
+
+frutas.append("morango")
+frutas.insert(1, "pera")
+print("Após adicionar:", frutas)
+
+frutas.remove("uva")
+ultima = frutas.pop()
+print("Após remover 'uva' e pop():", frutas, "Última removida:", ultima)
