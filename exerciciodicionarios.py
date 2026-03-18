3c4f4aa24da6ede53673f01cbcf6bb3d742c2eab
Nicollemedeir0s <medeirosnicolle03@gmail.com>
2026-03-18 19:40:29 -0300

    Add files via upload

---
+++ exerciciodicionarios.py
@@ -0,0 +1,17 @@
+aluna = {
+    "nome": "Maria",
+    "idade": 20,
+    "nota": 8.5
+}
+
+print(aluna)
+
+print(aluna["nome"])
+
+aluna["turma"] = "A"
+
+print(aluna["turma"])
+
+aluna["nota"] = 9.0
+
+print(aluna["nota"])
